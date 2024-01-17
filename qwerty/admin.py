042c4f21ff67ae4f51a24e562790cde1cbabad22
uscherbnyy <uscherbnyy@gmail.com>
2024-01-17 23:52:39 +0300

    Создайте модели Task и User, регистраци моделей в админке, и занос приложения в сетинги что забылось в начале

--- qwerty/admin.py
+++ qwerty/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from qwerty.models import User, Task
+
+admin.site.register(User)
+admin.site.register(Task)
--- qwerty/models.py
+++ qwerty/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+"""Создайте модели Task и User"""
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
+
+
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ('В ожидании', 'В ожидании'),
+        ('В процессе', 'В процессе'),
+        ('Завершено', 'Завершено')
+    )
+
+    task_name = models.CharField(max_length=200)
+    description = models.TextField()
+    assignee = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    created_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.task_name
+
+    class Meta:
+        verbose_name = "задача"
+        verbose_name_plural = "задачи"
