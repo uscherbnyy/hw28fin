b5884127db44f6d61da98a92dec810d689ff72a9
uscherbnyy <uscherbnyy@gmail.com>
2024-01-18 01:03:29 +0300

    Создали представление (view) для отображения списка всех задач на главной странице. Задачи отсортированы по дате создания в обратном порядке (самые новые задачи должны быть вверху).Прокинули url и изменили язык

--- qwerty/urls.py
+++ qwerty/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from qwerty import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.task_list),
 
 ]
--- qwerty/views.py
+++ qwerty/views.py
@@ -1,8 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from qwerty.models import Task
+
 # Create your views here.
 
 
-def home(request):
-    return HttpResponse('сдеся будя сайт')
+"""
+Создайте представление (view) для отображения списка всех задач на главной странице. Задачи должны быть отсортированы 
+по дате создания в обратном порядке (самые новые задачи должны быть вверху).
+"""
+
+
+def task_list(request):
+    tasks = Task.objects.order_by('-created_date')
+    context = {
+        'tasks': tasks,
+    }
+    return render(request, 'task_list.html', context)
